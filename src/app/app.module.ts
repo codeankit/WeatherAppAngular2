6fc384992852b9d93c8043414425564e626a2aa4
codeankit <ankitmawande17@gmail.com>
2017-09-11 01:53:17 -0400

    Weather App Angular2

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { WeatherService } from './weather.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  providers: [ WeatherService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
